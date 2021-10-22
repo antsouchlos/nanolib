16c7073b0e51e206bf2ac754ea19e8faf30a576f
Andreas Tsouchlos <an.tsouchlos@gmail.com>
2021-10-22 04:14:48 +0200

    Added Gpio example

---
+++ examples/gpio.cpp
@@ -0,0 +1,16 @@
+#include <arduino_lib/Gpio.h>
+
+
+using namespace periph;
+
+
+int main() {
+	Gpio<Port::B, Pin::_4, Direction::in>  input;
+	Gpio<Port::B, Pin::_5, Direction::out> led;
+
+	while(true) {
+		led.write(input.read());
+	}
+
+	return 0;
+}
