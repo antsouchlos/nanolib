fa34c375212799a85b6d46169ec9dd52bc55eb42
Andreas Tsouchlos <an.tsouchlos@gmail.com>
2021-11-02 18:29:05 +0100

    Added basic_writing and no_overflow writing tests

--- unit_tests/register_value.cpp
+++ unit_tests/register_value.cpp
@@ -8,23 +8,94 @@ using namespace periph::periph_detail;
 namespace {
 
 
-uint_single_t REG1_buffer[sizeof(uint_single_t)*8] = {0};
+    /*
+     *
+     * Emulate the hardware registers with simple buffers
+     *
+     */
 
-struct register_set {
-    struct REG1 {
-        static std::size_t address;
 
-        using VAL1 = RegisterValue<REG1, 0, 8>;
+    uint8_t REG1_buffer[2] = {0};
+    uint8_t REG2_buffer[2] = {0};
+    uint8_t REG3_buffer[2] = {0};
+
+    struct register_set {
+        struct REG1 {
+            static std::size_t address;
+            using VAL1 = RegisterValue<REG1, 0, 8>;
+        };
+
+        struct REG2 {
+            static std::size_t address;
+            using VAL1 = RegisterValue<REG2, 0, 4>;
+            using VAL2 = RegisterValue<REG2, 4, 4>;
+        };
+
+        struct REG3 {
+            static std::size_t address;
+            using VAL1 = RegisterValue<REG3, 0, 16>;
+        };
     };
-};
 
-std::size_t register_set::REG1::address = reinterpret_cast<std::size_t>(&REG1_buffer);
+    std::size_t register_set::REG1::address = reinterpret_cast<std::size_t>(&REG1_buffer);
+    std::size_t register_set::REG2::address = reinterpret_cast<std::size_t>(&REG2_buffer);
+    std::size_t register_set::REG3::address = reinterpret_cast<std::size_t>(&REG3_buffer);
 
-TEST(RegisterValue, write) {
-    constexpr uint_single_t val = 0b01010101;
-    register_set::REG1::VAL1::write<0b01010101>();
-    EXPECT_EQ(register_set::REG1::VAL1::read_as<uint_single_t>(), val);
-}
+
+    /*
+     *
+     * Actual tests
+     *
+     */
+
+
+    TEST(WriteTests, basic_write) {
+        constexpr uint8_t val1 = 0b01010101;
+        constexpr uint8_t val2 = 0b1101;
+        constexpr uint8_t val3 = 0b0111;
+        constexpr uint16_t val4 = 0b11110000'10011111;
+
+        for (auto& elem : REG1_buffer)
+            elem = 0b10111010;
+        for (auto& elem : REG2_buffer)
+            elem = 0b10101111;
+        for (auto& elem : REG3_buffer)
+            elem = 0b11100011;
+
+        register_set::REG1::VAL1::write<val1>();
+        EXPECT_EQ(REG1_buffer[0], val1);
+
+        register_set::REG2::VAL1::write<val2>();
+        register_set::REG2::VAL2::write<val3>();
+        EXPECT_EQ(REG2_buffer[0], val2 | (val3 << 4));
+
+        register_set::REG3::VAL1::write<val4>();
+        uint16_t reg_value = *(reinterpret_cast<uint16_t *>(REG3_buffer));
+        EXPECT_EQ(reg_value, val4);
+    }
+
+    TEST(WriteTests, no_overflow) {
+        constexpr uint8_t val1 = 0b01010101;
+        constexpr uint8_t val2 = 0b1101;
+        constexpr uint8_t val3 = 0b0111;
+
+        for (auto& elem : REG1_buffer)
+            elem = 0b11111111;
+        for (auto& elem : REG2_buffer)
+            elem = 0b11111111;
+        for (auto& elem : REG3_buffer)
+            elem = 0b11111111;
+
+        register_set::REG1::VAL1::write<val1>();
+        EXPECT_EQ(REG1_buffer[1], 0b11111111);
+
+        register_set::REG2::VAL1::write<val2>();
+        register_set::REG2::VAL2::write<val3>();
+        EXPECT_EQ(REG2_buffer[1], 0b11111111);
+
+        register_set::REG3::VAL1 ::write<val3>();
+        EXPECT_EQ(REG3_buffer[1], 0);
+    }
 
 
 }
