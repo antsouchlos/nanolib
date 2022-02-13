e9fe293a78a9c3796f188e51c6b1580c7de1b8e8
Andreas Tsouchlos <an.tsouchlos@gmail.com>
2022-02-13 10:11:30 +0100

    Fixed custom_conf example

--- examples/src/usart/custom_conf.cpp
+++ examples/src/usart/custom_conf.cpp
@@ -5,16 +5,17 @@ using namespace periph;
 
 
 // clang-format off
-using custom_usart_conf = usart_conf_t<ClockPolarity::async,
+using custom_usart_conf = usart_conf_t<ClockPolarity::tx_falling_rx_rising,
                                       StopBits::_2,
-                                      ParityMode::enabled,
-                                      UsartMode::async,
-                                      CharacterSize::_7>;
+                                      ParityMode::even,
+                                      UsartMode::sync,
+                                      CharacterSize::_8>;
 // clang-format on
 
 
 int main() {
-    Usart<Baudrate::_115_2_KHz, custom_usart_conf> usart;
+    System& system = System::get_instance();
+    Usart<Baudrate::_115_2_kHz, custom_usart_conf> usart{system};
 
     while (true) {
         while (!usart.char_pending())
