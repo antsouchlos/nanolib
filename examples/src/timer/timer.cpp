246036a3101690ce65e56f7ff7d51dacca5193dd
Andreas Tsouchlos <an.tsouchlos@gmail.com>
2022-02-17 22:32:20 +0100

    Boilerplate to be able to use timer, usart and led

--- examples/src/timer/timer.cpp
+++ examples/src/timer/timer.cpp
@@ -1,11 +1,24 @@
+#define NANOLIB_HW_OSC_FREQ_Hz (uint32_t)(16000000u)
+#define NANOLIB_CLOCKDIV       ClockDivisionFactor::_1
+
+#include <nanolib/Gpio.h>
+#include <nanolib/System.h>
 #include <nanolib/Timer.h>
+#include <nanolib/Usart.h>
 
 
 using namespace periph;
 
 
 int main() {
-    Timer<TimerModule::_0> timer;
+    System& system = System::get_instance();
 
-    return 0;
+    Timer<TimerModule::_0>                 timer;
+    Gpio<Port::B, Pin::_5, Direction::out> led;
+    Usart<Baudrate::_1_MHz>                usart{system};
+
+    while (true) {
+        usart.write(usart.read());
+        led.write(!led.read());
+    }
 }
