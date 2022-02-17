1d0aaceceaf4f13db8ef67fecd507e1ca809ad5c
Andreas Tsouchlos <an.tsouchlos@gmail.com>
2022-02-17 20:51:10 +0100

    Examples now working

--- examples/src/usart/custom_conf.cpp
+++ examples/src/usart/custom_conf.cpp
@@ -1,3 +1,7 @@
+#define NANOLIB_HW_OSC_FREQ_Hz (uint32_t)(16000000u)
+#define NANOLIB_CLOCKDIV       ClockDivisionFactor::_1
+
+
 #include <nanolib/Usart.h>
 
 
@@ -5,23 +9,20 @@ using namespace periph;
 
 
 // clang-format off
-using custom_usart_conf = usart_conf_t<ClockPolarity::tx_falling_rx_rising,
-                                      StopBits::_2,
+using custom_usart_conf = usart_conf_t<ClockPolarity::async,
+                                      StopBits::_1,
                                       ParityMode::even,
-                                      UsartMode::sync,
+                                      UsartMode::async,
                                       CharacterSize::_8,
-                                      TransmissionSpeed::normal>;
+                                      TransmissionSpeed::double_>;
 // clang-format on
 
 
 int main() {
-    System& system = System::get_instance();
-    Usart<Baudrate::_115_2_kHz, custom_usart_conf> usart{system};
+    System&                           system = System::get_instance();
+    Usart<2000000, custom_usart_conf> usart{system};
 
     while (true) {
-        while (!usart.char_pending())
-            ;
-
         usart.write(usart.read());
     }
 
